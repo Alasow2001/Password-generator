4a275a7671fd264d07fc798417f5dfeebf2ca04e
Ahmed Alasow <a.alasow01@gmail.com>
2023-03-29 17:05:36 +0100

    Added comments to explain functionality if the code

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ var generateBtn = document.querySelector("#generate");
 // This function will control the validation of the users password
 function generatePassword(){
 
+  // Prompt asking the user how long the password should be
   var length = window.prompt("How long would you like your password to be?");
 
+  // Restarts the app if the number doesn't meet the requirements
   if(length < 8 || length > 128){
     window.alert("You must enter a valid number")
     return generatePassword();
   }
 
+  // Asks the user if they would like any of these characters
   if(length >= 8 || length <=128){
     var hasLowerChars = confirm("Would you like lower case characters?")
     var hasNumberChars = confirm("Would you like number characters?")
@@ -20,11 +23,13 @@ function generatePassword(){
     var hasSpecialChars = confirm("Would you like special case characters?")
   }
 
+  // Will ask the user to confirm at least on type of character if they have selected none of them
   if(hasNumberChars===false && hasNumberChars===false && hasNumberChars===false && hasNumberChars===false){
     window.alert("Please confirm at least one type of the criteria")
     return generatePassword();
   }
 
+  
   if(hasNumberChars){
     var hasNumberChars = ["0123456789"]
   }
@@ -41,10 +46,12 @@ function generatePassword(){
     var hasSpecialChars = ["!£$%^&*()@~#"]
   }
 
+  // Combining all of the options to create the password
   var generatedPassword = hasLowerChars+hasNumberChars+hasSpecialChars+hasUpperChars
 
   var password = ""
 
+  // For loop going through each of the options to give a randomly generated password based on the users request
   for(var i=0; i<length; i++){
     var randomGenratedPassword = generatedPassword[Math.floor(Math.random() * generatedPassword.length)]
     password += randomGenratedPassword;
